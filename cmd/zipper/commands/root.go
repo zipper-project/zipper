ecee8a42d53b8bc5888dc08732c4ee8254f49b52
BenfuneyPenny <1323775977@qq.com>
2018-03-10 11:57:57 +0800

    cmd

---
+++ cmd/zipper/commands/root.go
@@ -0,0 +1,57 @@
+// Copyright (C) 2017, Zipper Team.  All rights reserved.
+//
+// This file is part of zipper
+//
+// The zipper is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The zipper is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/zipper-project/zipper/zipper"
+)
+
+var (
+	cfgFile string
+)
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "zipper",
+	Short: "zipper is a layered cross-chain network",
+	Long:  `zipper is a layered cross-chain network, a distributed ledger`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		zip := zipper.NewZipper(cfgFile)
+		zip.Start()
+	},
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/zipper.yaml)")
+}
--- cmd/zipper/main.go
+++ cmd/zipper/main.go
@@ -15,4 +15,15 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-package cmd
+
+package main
+
+import (
+	"github.com/zipper-project/zipper/cmd/zipper/commands"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	commands.Execute()
+}
