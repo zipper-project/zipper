a0dc548eedb5daee234c4f044667ae70b93dcff5
BenfuneyPenny <1323775977@qq.com>
2018-03-20 13:17:01 +0800

    fix test contract

--- test/withdraw_lua_p2p/main.go
+++ test/withdraw_lua_p2p/main.go
@@ -190,8 +190,7 @@ func invokeTx(privkey *crypto.PrivateKey, assetID uint32, amount int64, contract
 	sender := account.PublicKeyToAddress(*privkey.Public())
 
 	contractSpec := new(proto.ContractSpec)
-	contractSpec.Code = contractAddr.Bytes()
-
+	contractSpec.Addr = contractAddr.Bytes()
 	contractSpec.Params = args
 
 	tx := proto.NewTransaction(
